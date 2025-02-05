d8fd6f479c66b3a14769b661c8233ff73ca49fe2
swagnik <swagiksom3@gmail.com>
2025-02-05 20:34:03 +0530

    second_commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 
-// const instrument=require('./config/instrument.js')
-require("./config/instrument.js");
-const Sentry = require("@sentry/node");
+
+// require("./config/instrument.js");
+// const Sentry = require("@sentry/node");
 const express=require("express")
 const dotenv=require('dotenv')
 const cors=require('cors')
@@ -11,11 +11,11 @@ const app=express()
   connectDB()
 app.use(cors())
 app.use(express.json())
-app.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("My first Sentry error!");
-  });
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//     throw new Error("My first Sentry error!");
+//   });
   app.post('/webhooks',clerkhooks)
-Sentry.setupExpressErrorHandler(app);
+// Sentry.setupExpressErrorHandler(app);
 const PORT=process.env.PORT || 5000
 
 app.listen(PORT,()=>{
